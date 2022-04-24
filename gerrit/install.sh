1dcbeb4e04e96fefe160872db1249e542d0e0755
sunquan <sunquana@gmail.com>
2022-04-24 12:51:21 +0800

    feat: update net

--- gerrit/install.sh
+++ gerrit/install.sh
@@ -1,5 +1,11 @@
 !/bin/sh
 
-mkdir -p /data/gerrit
-cp -r conf /data/gerrit/
+## 切换root
+sudo su
 
+## 创建网络
+docker network create gerrit_default --subnet 192.168.20.0/24 --gateway 192.168.20.1
+
+## 创建数据目录
+mkdir -p /data/gerrit
+cp -r etc /data/gerrit/
